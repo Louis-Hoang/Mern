751126fd291f73b6593906feede2218bb9d40d32
Louis-Hoang <hoang_t1@denison.edu>
2023-09-10 16:42:28 -0400

    further config

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,71 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const mongoose = require("mongoose");
+const flash = require("connect-flash");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const methodOverride = require("method-override");
 
+const port = process.env.PORT || 5000;
+
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/musicList";
+
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("DB conencted");
+});
+
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    touchAfter: 24 * 60 * 60,
+    crypto: {
+        secret: "thisshouldbeabettersecret!",
+    },
+});
+
+const sessionConfig = {
+    store,
+    name: "session",
+    secret: "keyword",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
+};
+app.use(session(sessionConfig));
+app.use(flash());
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+    // res.header("Access-Control-Allow-Origin", "*");
+    // res.header(
+    //     "Access-Control-Allow-Headers",
+    //     "Origin, X-Requested-With, Content-Type, Accept"
+    // );
+    // next();
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    next();
+});
+
+app.use((req, res, next) => {
+    res.send("Welcome to Express");
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
